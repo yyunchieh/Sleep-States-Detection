789413b6a9f8273837bf12e13de5aa18e8f945c2
Jaslene <yunchien0102@gmail.com>
2024-11-20 16:56:00 +0800

    update

--- evd/utils.py
+++ evd/utils.py
@@ -5,4 +5,4 @@ def calculate_confidence_score(output):
   
     probabilities = F.softmax(output, dim=-1)
     confidence_score = torch.max(probabilities).item()  
-    return confidence_score
+    return confidence_score 
